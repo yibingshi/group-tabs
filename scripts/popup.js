85b177f673f6b93938e3b53f12ece90c0e83ba02
Yibing Shi <yibingshi94@gmail.com>
2019-01-03 11:25:34 -0600

    Feature branch - pinned tab & bookmark (#4)
    * Add bookmark feature
    
    * Add pinned feature (#3)

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,7 +2,7 @@
 
 const DOMAIN_REGEX = /(?:[-a-zA-Z0-9@:%_\+~.#=]{2,256}\.)?([-a-zA-Z0-9@:%_\+~#=]*)\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/i
 const DOMAIN_LENGTH = 20
-const TITLE_LENGTH = 47
+const TITLE_LENGTH = 37
 
 function promiseQuery(options){
   return new Promise(function(resolve,reject){
@@ -16,6 +16,7 @@ window.onload = function() {
   let tab_count = 0;
   promiseQuery({currentWindow: true})
   .then(function(tabs){
+    console.log(tabs);
     tabs.forEach((tab) => {
       if (tab.url.includes('.') === false) { return; }
       tab_count += 1;
@@ -30,20 +31,28 @@ window.onload = function() {
     document.getElementById('footer').innerHTML = `Total Tabs: ${tab_count}`
   })
   .then(function() {
-    Object.keys(domains).sort().forEach((key) => {
+    Object.keys(domains).sort(comparePinned).forEach((key) => {
       var all_tabs = '';
 
       domains[key].forEach((tab) => {
         ids.push(tab.id);
+        var pinned = tab.pinned ? 'pinned-tab' : 'tab';
+        var pin_icon = tab.pinned ? '../images/pinned.png' : '../images/pin.png'
         all_tabs +=
           `<tr id=${tab.id}>` +
             '<td>' +
               `<input class="hiddenUrl" value=${tab.url}>` +
               `<img class="icon" src=${tab.favIconUrl || '../images/tab.png'} />` +
-              `<span class="tab">${tab.title.substring(0, TITLE_LENGTH)}</span>` +
+              `<span class=${pinned}>${tab.title.substring(0, TITLE_LENGTH)}</span>` +
             '</td>' +
             '<td>' +
               '<img class="copy" src="../images/copy.png" />' +
+            '</td>' +
+            '<td>' +
+              `<img class="highlight" src=${pin_icon} />` +
+            '</td>' +
+            '<td>' +
+              `<img class="bookmark" src='../images/star.png' />` +
             '</td>' +
             '<td>' +
               '<img class="close" src="../images/close.png" />' +
@@ -74,6 +83,18 @@ window.onload = function() {
           let url_div = element.firstElementChild.firstElementChild;
           url_div.select();
           document.execCommand("copy");
+        } else if (event.target.className == 'highlight'){
+          chrome.tabs.get(Number(element.id), function(tab){
+            chrome.tabs.update(Number(element.id), {'pinned': !tab.pinned}, function(){
+              window.location.reload();
+            });
+          });
+        } else if (event.target.className == 'bookmark'){
+          chrome.tabs.get(Number(element.id), function(tab){
+            chrome.bookmarks.create({'title': tab.title, 'url': tab.url}, function(){
+              window.location.reload();
+            });
+          });
         } else if (event.target.className == 'close'){
           chrome.tabs.remove(Number(element.id), function(){
             var removed = document.getElementById(element.id);
@@ -119,4 +140,12 @@ function Camelcase(str) {
     return null;
   }
   return str[0].toUpperCase() + str.slice(1);
+}
+
+function comparePinned(tabA, tabB) {
+  if (!tabA.pinned && tabB.pinned)
+     return -1;
+  if (tabA.pinned && !tabB.pinned)
+    return 1;
+  return 0;
 }
